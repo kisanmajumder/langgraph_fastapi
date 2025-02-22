57d0daa96505b9f5441080e1654fdaadbda11696
kisan93 <kisan.sonnet@gmail.com>
2025-02-22 10:42:37 +0530

    removed openai fetch

--- app/main.py
+++ app/main.py
@@ -2,6 +2,7 @@
 from fastapi import FastAPI
 from graph import agent
 from pydantic import BaseModel
+from typing import Union
 
 app = FastAPI()
 
@@ -12,9 +13,13 @@ class ChatInput(BaseModel):
 def health_check():
     return {'health_check': 'OK'}
 
-@app.post("/chat")
-async def chat(input: ChatInput):
-    response = await agent.graph.ainvoke({"messages": [{"role": "user", "content": input.messages}]})
-    return response["messages"][-1].content
+# @app.post("/chat")
+# async def chat(input: ChatInput):
+#     response = await agent.graph.ainvoke({"messages": [{"role": "user", "content": input.messages}]})
+#     return response["messages"][-1].content
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Union[str, None] = None):
+    return {"item_id": item_id, "q": q}
     
 
